e4817b0edbf92e0d6d0c89e9c80fd35cfbb0c426
valgring95 <33157@student.pwsztar.edu.pl>
2021-03-21 11:27:37 +0100

    adding navbar-component and create navbar.js

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+let nav = '<a href="index.html">Strona główna</a>\n' +
+    '\n' +
+    '    <a href="form.html">Ankieta</a>\n' +
+    '\n' +
+    '    <a href="contact.html">Kontakt</a>'
+let navbarElem = document.querySelector(".navbar");
+
+navbarElem.innerHTML = nav;
